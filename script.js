cece3cc77da43920d9ffd612570abf41412e55d2
hieu-ng <hieutnguyen06@gmail.com>
2020-05-21 10:29:11 +1000

    Adding basic functionality of a video player

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ const play = document.getElementById('play');
 const stop = document.getElementById('stop');
 const progress = document.getElementById('progress');
 const timestamp = document.getElementById('timestamp');
+
+video.addEventListener('click', toggleVideoStatus);
+video.addEventListener('pause', updatePlayIcon);
+video.addEventListener('play', updatePlayIcon);
+video.addEventListener('timeupdate', updateProgress);
+
+play.addEventListener('click', toggleVideoStatus);
+stop.addEventListener('click', stopVideo);
+progress.addEventListener('change', setVideoProgress);
